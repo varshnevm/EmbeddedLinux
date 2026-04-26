0b7d76ac17162cbd0f413f18395dc6e74e064729
Varshne VM <140084805+varshnevm@users.noreply.github.com>
2026-04-26 13:27:22 +0530

    Update forkreturn.c to include output example

--- forkreturn.c
+++ forkreturn.c
@@ -7,3 +7,13 @@ int main(){
 	printf("\nWelcome: My process id:%d, My parent's id:%d",getpid(),getppid());
 	return 0;
 }
+
+
+
+output:
+varsh@varshne:~/emblinux$ ./forkreturn
+
+Fork returns:931
+Welcome: My process id:930, My parent's id:312
+Fork returns:0
+Welcome: My process id:931, My parent's id:310
